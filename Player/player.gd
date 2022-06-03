c78469538feb26fe0a326c899749319ec0fdfc52
kaike95 <kaikedrift@hotmail.com>
2022-06-02 22:07:27 -0300

    add

--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,21 @@
 extends KinematicBody2D
 
+export var vida : int = 10
+
 const ACCEL = 500
 const MAX_SPEED = 100
 const FRICTION = 400
 var velocity = Vector2.ZERO
 
+signal damage_received(life)
+
 onready var animationplayer = $AnimationPlayer
 onready var animationtree = $AnimationTree
 onready var animationstate = animationtree.get("parameters/playback")
+
+func death():
+	if vida == 0:
+		queue_free()
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -23,5 +31,13 @@ func _physics_process(delta):
 	else:
 		animationstate.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta ) 
-
+	death()
 	move_and_slide(velocity)
+
+
+
+func _on_hurtbox_area_entered(hitbox):
+	print("DEBUG ",vida - 1)
+	print("DEBUG:hitbox de " + hitbox.get_parent().name + " tocou " + name + " e deu 1 de dano")
+	emit_signal("damage_received", vida)
+	vida = vida -1
